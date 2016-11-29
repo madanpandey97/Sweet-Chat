8822104e3d0dbf974787597cefd12d31da06d4ef
madan <madanpandey97>
2016-11-30 01:47:11 +0530

    file added

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+$host="localhost";
+$user="root";
+
+$pass="";
+$db_name="chat";
+
+$db=new mysqli($host,$user,$pass,$db_name);
+
+function formatDate($date){
+	return date('g:i a',strtotime($date));
+}
+
+?>
